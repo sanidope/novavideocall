21dcfe399c75245432c8cdf092d4690073a6ea22
sani <sani25573@gmail.com>
2023-05-08 16:12:01 +0100

    changed settings file

--- src/videocall/settings/local.py
+++ src/videocall/settings/local.py
@@ -15,7 +15,7 @@ BASE_URL = 'http://127.0.0.1:8000'
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config['SECRET_KEY']
+SECRET_KEY = "django-insecure-5b@qjt3lrh$e4#84o16!8v##h&@pst+q$le8=c0#_hxja)v=9i"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -93,27 +93,15 @@ WSGI_APPLICATION = 'videocall.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
 '''
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config['DB_NAME'],
-        'USER': config['DB_USER'],
-        'PASSWORD': config['DB_PASS'],
-        'HOST': '127.0.0.1',
-        'PORT': '5432'
-    }
-}
-'''
+
 
 
 # Password validation
